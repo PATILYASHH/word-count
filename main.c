1be7f535fae2a838bc82377886d8e119650a417c
PATILYASHH <149749700+PATILYASHH@users.noreply.github.com>
2024-02-19 15:42:54 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,16 @@
+// Day 9 of 75 Days
+
+#include<stdio.h>
+int main()
+{
+  char string[100];
+
+  printf("Enter Your Text Here : ");
+  scanf("%s",&string);
+
+    char  length = strlen(string);
+
+  printf("%d",length);
+
+return 0;
+}
